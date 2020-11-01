98182a63de957803cfbe5e464c727d9cc76c181f
KIMYOUNGHAN <yh22953905@gmail.com>
2020-11-01 18:51:04 +0900

    Inflearn 스프링과 JPA기반 웹 애플리케이션 개발 - 백기선님 - 회원 가입 -- 계정 도메인 2

--- src/main/java/me/kimyounghan/studyworld/domain/Account.java
+++ src/main/java/me/kimyounghan/studyworld/domain/Account.java
@@ -6,7 +6,10 @@ import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
-@Getter @Setter @EqualsAndHashCode(of = "id")
+@Getter @Setter
+@EqualsAndHashCode(of = "id")
+// 연관관계가 복잡해질 때 EqualsAndHashCode에서 서로 다른 연관관계를 계속해서 순환 참조하느라 무한 루프가 발생하고
+// 결국엔 StackOverFlow가 발생할 수 있다.
 @Builder @AllArgsConstructor @NoArgsConstructor
 public class Account {
 
@@ -14,7 +17,7 @@ public class Account {
     private Long id;
 
     @Column(unique = true)
-    private String email;
+    private String email; // String은 기본적으로 varchar(255)에 매핑됨
 
     @Column(unique = true)
     private String nickname;
@@ -35,7 +38,8 @@ public class Account {
 
     private String location;
 
-    @Lob @Basic(fetch = FetchType.EAGER)
+    @Lob // varchar(255)보다 길어질 때
+    @Basic(fetch = FetchType.EAGER) // 바로바로 가져옴.
     private String profileImage;
 
     private boolean studyCreatedByEmail;
